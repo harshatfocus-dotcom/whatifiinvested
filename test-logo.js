96699c2d76945ae9acef027edc59829687489b54
Harsh Baid <harshbaid@Harshs-MacBook-Air.local>
2026-04-06 04:53:51 +0530

    chore: cleanup test files

--- test-logo.js
+++
@@ -1,6 +0,0 @@
-const https = require('https');
-https.get('https://autocomplete.clearbit.com/v1/companies/suggest?query=Reliance%20Industries', (res) => {
-  let data = '';
-  res.on('data', chunk => data += chunk);
-  res.on('end', () => console.log(data));
-});
